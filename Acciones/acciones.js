bc2765d3f4c92a7b2e16ef24f947a2e06aad20cd
Matias <matias@yopmail.com>
2023-01-30 23:06:02 -0300

    Lo mismo anterior

--- Acciones/acciones.js
+++ Acciones/acciones.js
@@ -18,23 +18,3 @@ function informacionExtra(ad) {
   console.log(ad)
   document.getElementById(ad).style.display = "inline-block"
 }
-/*
-function informacionExtraEA(ad2) {
-  var highlighted2 = document.getElementsByClassName("extraInfoEA")
-  for (var i = 0; i < 1; i++) {
-    console.log(i)
-    highlighted2[i].style.display = "none"
-  }
-  console.log(ad2)
-  document.getElementById(ad2).style.display = "inline-block"
-}
-
-function informacionExtraEL(ad3) {
-  var highlighted3 = document.getElementsByClassName("extraInfoEL")
-  for (var i = 0; i < 1; i++) {
-    console.log(i)
-    highlighted3[i].style.display = "none"
-  }
-  console.log(ad3)
-  document.getElementById(ad3).style.display = "inline-block"
-}*/
